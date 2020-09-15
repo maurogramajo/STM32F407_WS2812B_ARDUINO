6ad378e052d13fbb1f2f74de73313e5323672fb8
Mauro <grama_mauro@live.com.ar>
2020-09-15 00:57:19 -0300

    FREERTOS E INTERRUPCIONES
    Uso freertos e interrupciones para usar el encoder y manejar 10 leds ws2812b.

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,105 @@
 #include <Arduino.h>
+#include <STM32FreeRTOS.h>
+#include <Adafruit_NeoPixel.h>
+#include <Rotary.h>
 
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
 
+//MATRIZ LED
+#define DIN_WS2812  PE2
+// How many NeoPixels are attached?
+#define NUMPIXELS 10 // Popular NeoPixel ring size
+
+#define FILAS 10
+#define COLUMNAS 10
+int matriz[FILAS][COLUMNAS];
+
+Adafruit_NeoPixel pixels(NUMPIXELS, DIN_WS2812, NEO_GRB + NEO_KHZ800);
+
+//LEDS EN PLACA
 #define LED_1 PF10
 #define LED_2 PF9
 
 //ENCODER
 #define DT PE0
 #define CLK PE1
-//#define SWITCH 3
+//#define SWITCH PE3
 
-int anterior = 0;
-volatile int posicion = 0;
+// Counter that will be incremented or decremented by rotation.
+volatile int counter = 0;
 
-void encoder(void);
+  
+// Rotary encoder is wired with the common to ground and the two
+// outputs to DT and CLK.
+Rotary rotary = Rotary(CLK, DT);
 
-void setup()
-{
-  // initialize LED digital pin as an output.
+//FREERTOS
+
+/*
+ * TAREA MANEJO DE ENCODER POR POOLEO
+ */
+static void task_encoder(void* arg) {
+  UNUSED(arg);
+
+  // Counter that will be incremented or decremented by rotation.
+  int posicion = 0;
+
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
-
-  pinMode(DT,INPUT);
-  pinMode(CLK,INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(DT), encoder, LOW);
 
   digitalWrite(LED_1, HIGH); //EL LED_1 SE APAGA CON UN HIGH
   digitalWrite(LED_2, HIGH);  //EL LED_2 SE APAGA CON UN HIGH
 
-}
-
-void loop()
-{
-  if(posicion != anterior)
-    anterior = posicion;
-
-  switch(anterior){
-    case 0:
-      digitalWrite(LED_1, LOW);
-      delay(1000);
-      digitalWrite(LED_1, HIGH);
-      delay(1000);
-      break;
-    case 1:
-      digitalWrite(LED_2, LOW);
-      delay(500);
-      digitalWrite(LED_2, HIGH);
-      delay(500);
-      break;
-    default:
-      break;
-  }
-}
-
-void encoder(){
-  static unsigned long ultimainterr = 0;
-  unsigned long tiempointerr = millis();
-  
-  if(tiempointerr - ultimainterr > 5){
-    if(digitalRead(CLK) == HIGH)
-        posicion--;
-    else
-        posicion++;
-
-    if(posicion < 0){
-      posicion = 0;
-    }else if(posicion > 1){
-      posicion = 1;
+  //LLENO MATRIZ
+  for(int i=0; i < FILAS; i++){
+    for(int j=0; j < COLUMNAS; j++){
+      matriz[i][j] = (i*10)+j;
     }
-
-    ultimainterr = tiempointerr;
   }
+
+  pixels.begin();
+
+  pixels.clear();
+  pixels.setPixelColor(matriz[0][posicion], pixels.Color(75,50,0));
+  pixels.show();
+
+  while (1) {
+    if(counter != posicion){
+      posicion = counter;
+      pixels.clear();
+      pixels.setPixelColor(matriz[0][posicion], pixels.Color(75,50,0));
+      pixels.show();
+    }
+    // Sleep for 200 milliseconds.
+    vTaskDelay((10L * configTICK_RATE_HZ) / 1000L);
+  }
+}
+
+void rotate(void);
+
+void setup()
+{
+  attachInterrupt(DT, rotate, CHANGE);
+  attachInterrupt(CLK, rotate, CHANGE);
+  // create task at priority one
+  xTaskCreate(task_encoder, (const char *)"task_encoder", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
+  // start scheduler
+  vTaskStartScheduler();
+
+  while (1);  
+}
+
+void loop(){
+}
+
+// rotate is called anytime the rotary inputs change state.
+void rotate() {
+  unsigned char result = rotary.process();
+  if (result == DIR_CW) {
+    counter++;
+  } else if (result == DIR_CCW) {
+    counter--;
+  }
+  counter = MIN(9,MAX(0,counter)); 
 }
